433e0e170c3c5c4c2e52d7a1a1f3efb7613adacb
glennc <glennc@microsoft.com>
2015-07-15 11:12:58 -0700

    switch to less and remove babel on gulpfile so that the solution works better in VS. change UL to panels.

--- src/DesignBoard/gulpfile.babel.js
+++
@@ -1,32 +0,0 @@
-﻿var gulp = require('gulp');
-var bower = require('main-bower-files'),
-    sass = require('gulp-sass'),
-    project = require('./project.json');
-
-var config = {     sassPath: './Client/Styles',     bowerDir: './bower_components'
-}
-
-gulp.task('default', ['bower', 'icons', 'sass']);
-
-gulp.task('bower', () => {
-    return gulp.src(bower(), { base: config.bowerDir })
-               .pipe(gulp.dest(`./${project.webroot}/lib`));
-});
-
-gulp.task('sass', () => {
-    return gulp.src(`${config.sassPath}/styles.scss`)         .pipe(sass({
-            outputStyle: 'compressed',
-            includePaths: [
-                config.sassPath,
-                `${config.bowerDir}/bootstrap-sass-official/assets/stylesheets`,
-                `${config.bowerDir}/fontawesome/scss`,
-            ]}).on('error', sass.logError))         .pipe(gulp.dest(`${project.webroot}/css`));
-});
-
-gulp.task('watch', () => {
-    return gulp.watch('./Client/Styles/**/*.scss', ['sass']);
-});
-
-gulp.task('icons', () => {
-    return gulp.src(`${config.bowerDir}/fontawesome/fonts/**.*`)         .pipe(gulp.dest(`${project.webroot}/fonts`));
-});
---
+++ src/DesignBoard/gulpfile.js
@@ -0,0 +1,38 @@
+﻿var gulp = require('gulp');
+var bower = require('main-bower-files'),
+    less = require('gulp-less'),
+    minifyCSS = require('gulp-minify-css'),
+    project = require('./project.json');
+
+var config = {
+    lessPath: './Client/Styles',
+    bowerDir: './bower_components'
+}
+
+gulp.task('default', ['bower', 'icons', 'styles']);
+
+gulp.task('bower', function() {
+    return gulp.src(bower(), { base: config.bowerDir })
+               .pipe(gulp.dest(project.webroot + '/lib'));
+});
+
+gulp.task('styles', function () {
+    return gulp.src(config.lessPath + '/styles.less')
+               .pipe(less({
+                    paths: [
+                        config.lessPath,
+                        config.bowerDir + '/bootstrap/less',
+                        config.bowerDir + '/fontawesome/less'
+                        ]}))
+               .pipe(minifyCSS())
+               .pipe(gulp.dest(project.webroot + '/css'));
+});
+
+gulp.task('watch', function() {
+    return gulp.watch('./Client/Styles/**/*.less', ['styles']);
+});
+
+gulp.task('icons', function() {
+    return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*')
+        .pipe(gulp.dest(project.webroot + '/fonts'));
+});
