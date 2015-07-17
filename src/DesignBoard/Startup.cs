57a38d4388844dc3d3c5ab1b7656f47859173919
anurse <andrew@andrewnurse.net>
2015-07-17 16:40:09 -0700

    just a few touch-ups :)

--- src/DesignBoard/Startup.cs
+++ src/DesignBoard/Startup.cs
@@ -4,16 +4,33 @@ using System.Linq;
 using System.Threading.Tasks;
 using DesignBoard.Services;
 using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
+using Microsoft.Framework.Configuration;
 using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.Runtime;
 
 namespace DesignBoard
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment hostingEnvironment, IApplicationEnvironment applicationEnvironment)
+        {
+            Configuration = new ConfigurationBuilder(applicationEnvironment.ApplicationBasePath)
+                .AddJsonFile("config.json")
+                .AddJsonFile($"config.{hostingEnvironment.EnvironmentName}.json", optional: true)
+                .AddUserSecrets()
+                .AddEnvironmentVariables()
+                .Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddInstance(Configuration);
 
             services.AddSingleton<ISearchResultsProvider, SearchResultsProvider>();
         }
